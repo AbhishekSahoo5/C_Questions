269a74a102abbb8078ae10ba65f46983b6caf354
Abhishek Sahoo <abhisheksahoo553@gmail.com>
2023-12-20 22:37:52 +0530

    print m to n

--- Looping_Qs/03_print_MtoN.c
+++ Looping_Qs/03_print_MtoN.c
@@ -1,15 +1,34 @@
 #include <stdio.h>
 
 int main() {
-    int m=-10,n=20;
-    for(int i=m;i<=n;i++)
+    int m=-10,n=-20;
+
+    if(m>n)
     {
-        printf("%d ",i);
+        //forward
+        for(int i=n;i<=m;i++)
+        {
+            printf("%d ",i);
+        }
+        printf("\n");
+        //reverse
+        for(int i=m;i>=n;i--)
+        {
+            printf("%d ",i);
+        }
     }
-    printf("\n");
-    for(int i=n;i>=m;i--)
-    {
-        printf("%d ",i);
+    else if(n>m){
+        //forward
+        for(int i=m;i<=n;i++)
+        {
+            printf("%d ",i);
+        }
+        printf("\n");
+        //reverse
+        for(int i=n;i>=m;i--)
+        {
+            printf("%d ",i);
+        }
     }
 
     return 0;
