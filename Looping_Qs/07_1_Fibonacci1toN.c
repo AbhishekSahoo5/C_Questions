5799fd7b4a920f166b67ddc75e9e7f8095bff834
Abhishek Sahoo <abhisheksahoo553@gmail.com>
2023-12-20 23:34:11 +0530

    factorial

--- Looping_Qs/07_1_Fibonacci1toN.c
+++ Looping_Qs/07_1_Fibonacci1toN.c
@@ -1,16 +1,20 @@
 #include <stdio.h>
 
-int main() {
-    int n=10;
-    int n1=0;
-    int n2=1;
-    int nextNum;
-    for(int i=3;i<=10;i++)
+void fibonacci(int n){
+    int n1=0,n2=1,sum=0;
+    printf("%d ",n1);
+    // printf("%d ",n2);
+    while(n2<n)
     {
-        nextNum=n1+n2;
-        printf("%d ",nextNum);
+        sum=n1+n2;
+        printf("%d ",n2);
         n1=n2;
-        n2=nextNum;
+        n2=sum;
     }
+}
+
+int main() {
+    int n=20;
+    fibonacci(n);
     return 0;
 }
