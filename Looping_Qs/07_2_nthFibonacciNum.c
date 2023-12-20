123704dbfe9835f4ffa83675ba4f0c48e1245465
Abhishek Sahoo <abhisheksahoo553@gmail.com>
2023-12-20 23:45:48 +0530

    nth Fibonacci number

--- Looping_Qs/07_2_nthFibonacciNum.c
+++ Looping_Qs/07_2_nthFibonacciNum.c
@@ -1,8 +1,34 @@
 #include<stdio.h>
 
+int nthFibonacci(int n)
+{
+    int n1=0;
+    int n2=1;
+    int count=2,sum=0;
+    if(n==1)
+        return 0;
+    
+    else if(n==2)
+        return 1;
+    
+    else if(n<0)
+        return -1;
+    
+    else{
+         while(count<n)
+        {
+            sum=n1+n2;
+            n1=n2;
+            n2=sum;
+            count++;
+        }
+    }
+    return sum;
+}
+
 int main()
 {
-    int n;
-    int a=0;
+    int n=5;
+    printf("%d",nthFibonacci(n));
     
 }
