94ede151d45af1a99c7fcff388576c4878fbdb43
Abhishek Sahoo <abhisheksahoo553@gmail.com>
2023-12-20 22:32:16 +0530

    print n to 1

--- Looping_Qs/02_print_Nto1.c
+++ Looping_Qs/02_print_Nto1.c
@@ -4,8 +4,18 @@ int main(){
     int n;
     printf("Enter n");
     scanf("%d",&n);
-    for(int i=n;i>=1;i--)
+    if(n>0)
     {
-        printf("%d ",i);
+        for(int i=n;i>=1;i--)
+        {
+            printf("%d ",i);
+        }
+    }
+    if(n<0)
+    {
+        for(int i=n;i<=1;i++)
+        {
+            printf("%d ",i);
+        }
     }
 }
